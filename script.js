9fea28f003ac47358a4da3e11a265e9009c52500
EddyeBoy27 <lipe0503@gmail.com>
2019-12-11 08:41:58 -0200

    Finalizado Verificaçao de Nome e Sobrenome

--- script.js
+++ script.js
@@ -22,9 +22,7 @@ botaoEnviar.addEventListener('click', function () {
     campoEmailTelefone = document.getElementsByTagName('input')[0].value
     campoSenha = document.querySelectorAll('.loginItem')[1].querySelectorAll('input')[0].value
     if (campoSenha !== "" && validateEmail(campoEmailTelefone) == true || validateTelefone(campoEmailTelefone) == true) {
-        // if (campoSenha == "") {
-            alert("Login realizado com sucesso");
-        // }
+        alert("Login realizado com sucesso");
     } else {
         alert("Você não inseriu um e-mail ou número de telefone válido e/ou o campo senha está vazio");
     }
@@ -34,34 +32,63 @@ document.querySelector('#datePicker').DatePickerX.init({
     format: 'dd/mm/yyyy'
 });
 
-// $('#datepicker').datepicker({
-//     uiLibrary: 'bootstrap4'
-// })
-
 
 // Regex Nome
 
-function validateNome(nome) {
-    var resultado = /^[A-Za-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'-]+$/
-    return resultado.test(nome)
+function validateNome(event) {
+    let nomeValue = event.target.value
+    var resultado = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'-]+(\s{0,1})*$/;
+    if (resultado.test(nomeValue) == true) {
+        let bordaNome = document.querySelectorAll('.form-group')[0].querySelectorAll('.form-control')[0]
+        bordaNome.style.borderColor = 'greenyellow'
+    } else if (resultado.test(nomeValue) !== true && nomeValue == "") {
+        let bordaNome = document.querySelectorAll('.form-group')[0].querySelectorAll('.form-control')[0]
+        bordaNome.style.borderColor = 'red'
+    }
 }
 
 // Regex Sobrenome
 
 function validateNomeSobrenome(nomeSobrenome) {
+    let nomeValue = event.target.value
     var resultado = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
-    return resultado.test(nomeSobrenome)
-}
-
-function verificaNomeForm (nome){
-    campoNome = document.querySelectorAll('.form-group')[0].querySelector('.form-control').value
-    if (campoNome !== "" && validateNome(campoNome) == true){
-        return true
-    } else {
-        return false
+    if (resultado.test(nomeValue) == true) {
+        let bordaSobrenome = document.querySelectorAll('.form-group')[1].querySelectorAll('.form-control')[0]
+        bordaSobrenome.style.borderColor = 'greenyellow'
+    } else if (resultado.test(nomeValue) !== true && nomeValue == "") {
+        bordaSobrenome.style.borderColor = 'red'
     }
 }
 
+
 // Verifica Nome
 
-let campoNome = document.querySelectorAll('.form-group')[0].querySelector('.form-control')
+let bordaNome = document.querySelectorAll('.form-group')[0].querySelector('.form-control')
+
+// Eventos no Nome do Formulário
+
+bordaNome.addEventListener('click', validateNome)
+bordaNome.addEventListener('keypress', validateNome)
+bordaNome.addEventListener('keydown', validateNome)
+bordaNome.addEventListener('keyup', validateNome)
+bordaNome.addEventListener('changes', validateNome)
+
+// Verifica Sobrenome
+
+let bordaSobrenome = document.querySelectorAll('.form-group')[1].querySelectorAll('.form-control')[0]
+
+// Eventos no Sobrenome do Formulário
+
+bordaSobrenome.addEventListener('click', validateNomeSobrenome)
+bordaSobrenome.addEventListener('keypress', validateNomeSobrenome)
+bordaSobrenome.addEventListener('keydown', validateNomeSobrenome)
+bordaSobrenome.addEventListener('keyup', validateNomeSobrenome)
+bordaSobrenome.addEventListener('changes', validateNomeSobrenome)
+
+// Verifica Email
+
+bordaEmail = document.querySelectorAll('.form-group')[2].querySelectorAll('.form-control')[0]
+
+// Eventos no E-mail e Celular
+
+
